dd81385a9e8f78f76c0a9f21c060f06e2c7cb217
h <aa>
2024-04-29 14:01:37 +0800

    1

--- application/api/config.php
+++ application/api/config.php
@@ -4,6 +4,8 @@
 return [
     'exception_handle'        => '\\app\\api\\library\\ExceptionHandle',
     'default_return_type'    => 'json',
-    'image_url' => "https://api.rothpro.id",
-    'invite_url' => "https://www.rothpro.id"
+//    'image_url' => "https://api.rothpro.image_urld",
+//    'invite_url' => "https://www.rothpro.id",
+    'image_url' => "http://8.217.154.107:8088",
+    'invite_url' => "http://8.217.154.107:8089"
 ];
